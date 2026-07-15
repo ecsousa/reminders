8e8d68559aba78465a8755bc322e195bfd4f7b31
Eduardo Sousa <ecsousa@gmail.com>
2026-07-14 23:14:07 -0700

    Log absolute database path on application startup

--- backend/src/main/kotlin/com/reminders/config/AppConfig.kt
+++ backend/src/main/kotlin/com/reminders/config/AppConfig.kt
@@ -2,6 +2,9 @@ package com.reminders.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
+import java.io.File
 
 @Configuration
 @ConfigurationProperties(prefix = "reminders")
@@ -13,5 +16,13 @@ class AppConfig {
     class Dev {
         var username: String = "dev_user"
         var name: String = "Dev User"
+    }
+
+    private val logger = LoggerFactory.getLogger(AppConfig::class.java)
+
+    @PostConstruct
+    fun logDbPath() {
+        val dbFile = File(dbFolder, "reminders.db3")
+        logger.info("Using database file at: {}", dbFile.absolutePath)
     }
 }
